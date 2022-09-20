5c4536bbecc54ca824879de0b4587ac67a29fccd
Freddy Vega <114018907+FreddyVega18@users.noreply.github.com>
2022-09-20 18:15:02 +0000

    cambio falopa por mono

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 n = (int(input("Por favor, ingrese un numero, solo números enteros: ")))
 m = int(input("¿Cuántos multiplos?: "))
-  
+
 número = n  #numero entero a multiplicar
 desde = m  #desde que numero desé que multiplique
 
-for f in range (1,m+1):
-  print(f'{número} x {f} = {número * f}')
-  
+for f in range(1, m + 1):
+    print(f'{número} x {f} = {número * f}')
+  toy enoajo con vo 
